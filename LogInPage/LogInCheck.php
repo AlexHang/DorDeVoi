b46aec3e5811c9f65f4f8c0529c06fe432c23661
Alex Hang <yo3ijf@gmail.com>
2018-08-08 14:14:18 +0300

    Add files via upload

---
+++ LogInPage/LogInCheck.php
@@ -0,0 +1,67 @@
+<?php
+
+ session_start();
+
+$servername = "localhost";
+
+$username = "dordevoi_admin";
+
+$password = "Qerquzdwh87tV>N@f";
+
+$dbname = "dordevoi_database1";
+
+
+
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+} 
+
+$name="'".$_POST["username"]."'";
+
+$pass="'".$_POST["password"]."'";
+
+$sql = "SELECT * FROM `Users` WHERE UserName = ".$name." AND Password =".$pass." AND Validat = 1";
+
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+   
+
+    while($row = $result->fetch_assoc()) {
+            if ($row["Password"] == $_POST["password"])
+            { echo "Password is ok";
+
+    $_SESSION["connected"] = "yes";
+        $_SESSION["ID"]=$row["User_ID"];
+        echo "<script>window.top.location.href = 'https://dordevoi.ro/MyProfile/Profile.php';</script>"; 
+        echo $_SESSION["connected"]. $_SESSION["ID"];
+
+            }
+            
+            
+    }
+
+   
+
+   //
+
+} else {
+
+    echo "<center><h3 style='color:red'>Numele de utilizator/email-ul sau parola sunt gresite</h3></center>";
+
+}
+
+$conn->close();
+
+?>
